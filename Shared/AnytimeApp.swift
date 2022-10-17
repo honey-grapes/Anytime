ebee7146b906c3cad9660d72707ec5ce3a352afe
Josephine Chan <jcf1996chan@gmail.com>
2022-10-17 19:43:44 -0400

    Initial Commit

---
+++ Shared/AnytimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnytimeApp.swift
+//  Shared
+//
+//  Created by Josephine Chan on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AnytimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
